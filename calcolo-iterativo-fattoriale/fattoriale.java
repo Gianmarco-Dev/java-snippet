8fa8ebe22d95b8886d12da0b92ae5e764e0c9ec3
Gianmarco <gianmarco.venerito@gmail.com>
2021-01-06 04:45:44 +0100

    Create fattoriale.java
    Creazione metodo statico per il calcolo di una funzione fattoriale per numeri interi.
    - Specifiche e dati in commento

---
+++ calcolo-iterativo-fattoriale/fattoriale.java
@@ -0,0 +1,17 @@
+class Fattoriale {
+  /** Dati in ingresso: interi n>= 0
+  Dati in uscita: interi y>=0
+  Condizione di ingresso: true
+  Condizione di uscita: y = n!
+  */
+  public static void main (String[] args){
+    int n = 10;
+    int x = 0;
+    int y = 1;
+    while (x < n) {
+      x = x + 1 ;
+      y = x * y ;
+    }
+    System.out.println("Il fattoriale di " + n + " è: " + y)
+  }
+}
